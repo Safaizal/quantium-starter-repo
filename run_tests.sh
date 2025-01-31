3a90877eb246a35d205f854181a2cb6f71f4a893
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-01-31 22:46:47 +0530

    Make run_tests.sh excutable

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,19 @@
-source env/Scripts/activate
+#!/bin/bash
 
-pytest test_app.py
+# activate the virtual environment
+. ./venv/bin/activate
 
-EXIT_CODE=$?
+# run the test suite
+python -m pytest test_pink_morsel_visualizer.py
 
-deactivate
+# collect exit code from pytest
+# exit code is 0 if all tests pass
+PYTEST_EXIT_CODE=$?
 
-exit $EXIT_CODE
+# return exit code 0 if all tests pass or 1 otherwise
+if [ $PYTEST_EXIT_CODE -eq 0 ]
+then
+  exit 0
+else
+  exit 1
+fi
